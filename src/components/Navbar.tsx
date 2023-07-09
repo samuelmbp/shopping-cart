b080a0cd764287183dabd0ef9b0d1b061a7de912
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-09 20:02:49 +0100

    Display images for each product

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
