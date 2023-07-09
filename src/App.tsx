b38e7f7dea10732f74f806f7de8d9a80c9c02d9c
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-09 08:11:10 +0100

    Setup routing for home, about and store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Routes, Route } from "react-router-dom";
+import { Container } from "@chakra-ui/react";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import About from "./components/About";
+
+const App = () => (
+  <>
+    <Container marginBottom={4}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  </>
+);
 
 export default App;
